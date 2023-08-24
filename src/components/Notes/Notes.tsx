d0d18ae2aca29013d0196b22e79633a2c3564ee0
mjaumi <mjaumi2864@gmail.com>
2023-08-24 22:16:41 +0600

    removed some temporary codes

--- src/components/Notes/Notes.tsx
+++ src/components/Notes/Notes.tsx
@@ -2,20 +2,27 @@
 
 import React from 'react';
 import NoteItem from './NoteItem';
-import getNotes from '../../lib/getNotes';
-import NotesContainer from './NotesContainer';
 import { useGetNotesQuery } from '@/redux/features/notes/notesApi';
+import { useAppSelector } from '@/redux/hooks';
 
 const Notes = () => {
   // integration of RTK Query hooks here
   const { data: notes } = useGetNotesQuery(null);
+
+  // integration or react-redux custom hooks here
+  const noteData = useAppSelector((state) => state.note);
 
   // rendering the notes container component here
   return (
     <section className='w-full px-20 py-5'>
       <div className='pt-20'>
         <h1 className='text-5xl font-semibold'>Notes</h1>
-        <NotesContainer notes={notes} />
+        <div className='mt-20 grid grid-cols-4 gap-12'>
+          {noteData.note && <NoteItem note={noteData.note} />}
+          {notes?.map((note) => (
+            <NoteItem key={note.id} note={note} />
+          ))}
+        </div>
       </div>
     </section>
   );
